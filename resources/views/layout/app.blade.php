b95f5b7a8d0ddfbc7481fc5c88b767ebf022673a
faprayyy <fauzanjr1@gmail.com>
2022-01-04 01:42:17 +0700

    Memperbaiki Header

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -10,8 +10,6 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-    
-
     @yield('css')
     
     <!-- Custom styles for Panel Admin -->
@@ -32,11 +30,10 @@
            }
        }
    </style>
-
   </head>
   <body>
-
     @include('layout.admin.header')
+
     @yield('content')
 
 		<!-- Optional JavaScript -->
