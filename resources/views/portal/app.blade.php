7ffeecc15dd5c19cdf2261ede6619f0f9b090be8
faprayyy <fauzanjr1@gmail.com>
2022-01-07 16:43:37 +0700

    add assets medigo-template

---
+++ resources/views/portal/app.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<title>My Blog</title>
+    <meta name="keywords" content="">
+	<meta name="description" content="">
+    <meta name="author" content="templatemo">
+
+	<!-- Google Fonts -->
+	<link href="http://fonts.googleapis.com/css?family=PT+Serif:400,700,400italic,700itali" rel="stylesheet">
+	<link href="http://fonts.googleapis.com/css?family=Raleway:400,900,800,700,500,200,100,600" rel="stylesheet">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="/assets/bootstrap/bootstrap.css">
+	<link rel="stylesheet" href="/assets/css/misc.css">
+	<link rel="stylesheet" href="/assets/css/orange-scheme.css">
+	
+	<!-- JavaScripts -->
+	<script src="/assets/js/jquery-1.10.2.min.js"></script>
+	<script src="/assets/js/jquery-migrate-1.2.1.min.js"></script>
+
+	<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+
+</head>
+<body>
+    
+    @yield('content')
+
+    <!-- Scripts -->
+	<script src="/assets/js/min/plugins.min.js"></script>
+	<script src="/assets/js/min/medigo-custom.min.js"></script>
+
+</body>
+</html>
--- resources/views/portal/index.blade.php
+++ resources/views/portal/index.blade.php
@@ -1,4 +1,70 @@
-<h3>WELCOME TO MY BLOG</h3>
-<p>
-    Login <a href="{{ url('login') }}"> here!</a>
-</p>
+@extends('portal.app')
+
+@section('content')
+
+<div class="responsive_menu">
+    <ul class="main_menu">
+        <li><a href="index.html">Home</a></li>
+        <li><a href="#">Portfolio</a>
+            <ul>
+                <li><a href="portfolio.html">Portfolio Grid</a></li>
+                <li><a href="project-image.html">Project Image</a></li>
+                <li><a href="project-slideshow.html">Project Slideshow</a></li>
+            </ul>
+        </li>
+        <li><a href="#">Blog</a>
+            <ul>
+                <li><a href="blog.html">Blog Standard</a></li>
+                <li><a href="blog-single.html">Blog Single</a></li>
+                <li><a href="#">visit templatemo</a></li>
+            </ul>
+        </li>
+        <li><a href="archives.html">Archives</a></li>
+        <li><a href="contact.html">Contact</a></li>
+    </ul> <!-- /.main_menu -->
+</div> <!-- /.responsive_menu -->
+
+<header class="site-header clearfix">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="pull-left logo">
+                    <a href="index.html">
+                        <img src="/assets/images/logo.png" alt="Medigo">
+                    </a>
+                </div>	<!-- /.logo -->
+                <div class="main-navigation pull-right">
+                    <nav class="main-nav visible-md visible-lg">
+                        <ul class="sf-menu">
+                            <li><a href="index.html">Home</a></li>
+                            <li><a href="#">Portfolio</a>
+                                <ul>
+                                    <li><a href="portfolio.html">Portfolio Grid</a></li>
+                                    <li><a href="project-image.html">Project Image</a></li>
+                                    <li><a href="project-slideshow.html">Project Slideshow</a></li>
+                                </ul>
+                            </li>
+                            <li class="active"><a href="#">Blog</a>
+                                <ul>
+                                    <li><a href="blog.html">Blog Standard</a></li>
+                                    <li><a href="blog-single.html">Blog Single</a></li>
+                                    <li><a href="#">visit templatemo</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="archives.html">Archives</a></li>
+                            <li><a href="contact.html">Contact</a></li>
+                        </ul> <!-- /.sf-menu -->
+                    </nav> <!-- /.main-nav -->
+                    <!-- This one in here is responsive menu for tablet and mobiles -->
+                    <div class="responsive-navigation visible-sm visible-xs">
+                        <a href="#fauzan" class="menu-toggle-btn">
+                            <i class="fa fa-bars"></i>
+                        </a>
+                    </div> <!-- /responsive_navigation -->
+                </div> <!-- /.main-navigation -->
+            </div> <!-- /.col-md-12 -->
+        </div> <!-- /.row -->
+    </div> <!-- /.container -->
+</header> <!-- /.site-header -->
+
+@endsection
